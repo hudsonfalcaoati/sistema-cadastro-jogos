7f9048afed9f0e1db127f6a1508433c049b17f50
hudsonfalcao79 <47117454+hudsonfalcao79@users.noreply.github.com>
2023-09-13 13:08:36 -0300

    atualizando o init do projeto

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-console.jogos("Rodando o Sistema de Gerenciamento de Jogos")
+console.log("Rodando o Sistema de Gerenciamento de Jogos")
+console.log("Aplicação esta se conectando ao Bando de Dados")
