805f7e023b53925d638da289a80cbf12eab15279
hudson.falcao@etipi.pi.gov.br <hudson.falcao@etipi.pi.gov.br>
2023-09-15 12:34:25 -0300

    voltando para o estado anterior

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodando o Sistema de Gerenciamento de Jogos")
 console.log("Aplicação esta se conectando ao Bando de Dados")
 //mais um console log de teste 
 console.log("O Paulo fez esse commit sem você ver na aula")
-console.log("Enviando para o github")
