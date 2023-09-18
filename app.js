6206614b5087600b5a553c7a249ab8c65c1e09d9
hudson.falcao@etipi.pi.gov.br <hudson.falcao@etipi.pi.gov.br>
2023-09-18 12:12:09 -0300

    nova alteração

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o Sistema de Gerenciamento de Jogos")
 console.log("Aplicação esta se conectando ao Bando de Dados")
 //mais um console log de teste 
 console.log("O Paulo fez esse commit sem você ver na aula")
+console.log("Novo console")
